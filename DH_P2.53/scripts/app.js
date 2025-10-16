d6da96397f3e9a81225c361470f5dc11f857b976
oscarbrandsllc <oscarbrands.llc@gmail.com>
2025-10-15 17:25:24 -0700

    attempted fix, ng

--- DH_P2.53/scripts/app.js
+++ DH_P2.53/scripts/app.js
@@ -4423,7 +4423,6 @@ const wrTeStatOrder = [
                 }
 
                 playerComparisonModal.classList.remove('hidden');
-                playerComparisonModal.style.display = 'block';
                 if (comparisonBackgroundOverlay) {
                     comparisonBackgroundOverlay.classList.remove('hidden');
                 }
@@ -4442,7 +4441,6 @@ const wrTeStatOrder = [
                     modalContent.style.bottom = '';
                 }
                 playerComparisonModal.classList.add('hidden');
-                playerComparisonModal.style.display = 'none';
                 if (comparisonBackgroundOverlay) {
                     comparisonBackgroundOverlay.classList.add('hidden');
                 }
